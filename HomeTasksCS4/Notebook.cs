494f137b3246caf325a6cae6b7868e9f9665cb32
Jehun <121166619+Jehunn@users.noreply.github.com>
2023-01-25 20:04:15 +0400

    Hometask

---
+++ HomeTasksCS4/Notebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HomeTasksCS4
+{
+    internal class Notebook
+    {
+
+
+        public Notebook(string brand,string model,double price=0)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Price= price;
+
+        }
+
+        public string GroupInfo()
+        {
+            return $"{Brand}-{Model}-{Price}";
+        }
+
+
+
+        public string Brand;
+        public string Model;
+        public double Price;
+
+            
+
+
+
+
+
+
+
+    }
+    
+
+    
+}    
+
+
+
+    
+
---
+++ HomeTasksCS4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HomeTasksCS4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Notebook info = new Notebook("Razer", "Blade 15")
+            {
+                Price = 4000
+            };
+            //Console.WriteLine(info.GroupInfo());
+
+
+
+
+            Notebook[] nb = new Notebook[3];
+            
+            for(int i=0; i<nb.Length; i++)
+            {
+                Console.WriteLine("Mehsulun brand-ini daxil edin :");
+                string brand=Console.ReadLine();
+                
+       
+                Console.WriteLine("Mehsulun modelini daxil edin :");
+                string model= Console.ReadLine();
+
+                double price;
+                Console.WriteLine("Mehsulun qiymetini daxil edin :");
+                string priceStr = Console.ReadLine();
+                price = Convert.ToDouble(priceStr);
+
+                Notebook pr = new Notebook(brand, model,price);
+
+                nb[i]=pr;
+                
+
+               
+            }
+
+            for (int i = 0; i < nb.Length; i++)
+            {
+                Console.WriteLine(nb[i].GroupInfo());
+            }
+
+
+            double sum = 0;
+            for (int i = 0; i < nb.Length; i++)
+            {
+                sum += nb[i].Price;   
+            }
+            double average=sum/nb.Length;
+
+            Console.WriteLine(average);
+        }
+    }
+}
